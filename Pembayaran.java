868e063b5e352191c6c8dbaca3adb720cd77f316
rahmiaulia680-web <rahmiaulia680@gmail.com>
2026-06-18 22:06:18 +0700

    Pembayaran.java

---
+++ Pembayaran.java
@@ -0,0 +1,43 @@
+/*
+ * Nama : Rahmi Aulia
+ * NIM  : 2501083021
+*/
+package com.mycompany.praktikum21052026;
+
+/**
+ *
+ * @author Rahmi Aulia
+ */
+public abstract class Pembayaran {
+   protected double jumlahBayar;
+    protected String idTransaksi;
+    
+    public Pembayaran() {
+        
+    }
+
+    public Pembayaran(double jumlahBayar, String idTransaksi) {
+        this.jumlahBayar = jumlahBayar;
+        this.idTransaksi = idTransaksi;
+    }
+
+    public double getJumlahBayar() {
+        return jumlahBayar;
+    }
+
+    public void setJumlahBayar(double jumlahBayar) {
+        this.jumlahBayar = jumlahBayar;
+    }
+
+    public String getIdTransaksi() {
+        return idTransaksi;
+    }
+
+    public void setIdTransaksi(String idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+    
+    //konsep polymorphism
+    //abstract method
+    public abstract void prosesPembayaran(); 
+}
